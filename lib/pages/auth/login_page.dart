63934455c33ed724fc39cdde9c11b2100b587758
Admin <aakashshivgan7739@gmail.com>
2022-09-12 12:46:15 +0530

    Register page complete

--- lib/pages/auth/login_page.dart
+++ lib/pages/auth/login_page.dart
@@ -18,116 +18,119 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 80),
-          child: Form(
-            key: formKey,
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const Text(
-                    "Groupies",
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.w700,
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 80),
+            child: Form(
+              key: formKey,
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Groupies",
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.w700,
+                      ),
                     ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  const Text(
-                    "Login now to see what they are talking!",
-                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.w300),
-                  ),
-                  Image.asset("assets/groupie.jpg"),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  TextFormField(
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const Text(
+                      "Login now to see what they are talking!",
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.w300),
+                    ),
+                    Image.asset("assets/register.png"),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                        decoration: textInputDecoration.copyWith(
+                          labelText: "Email",
+                          prefixIcon: Icon(
+                            Icons.email_rounded,
+                            color: Constants().primaryColor,
+                          ),
+                        ),
+                        onChanged: (val) {
+                          setState(() {
+                            email = val;
+                          });
+                        },
+                        validator: ((val) {
+                          return RegExp(
+                                      r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                                  .hasMatch(val!)
+                              ? null
+                              : "Please enter a valid email";
+                        })),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    TextFormField(
                       decoration: textInputDecoration.copyWith(
-                        labelText: "Email",
+                        labelText: "Password",
                         prefixIcon: Icon(
-                          Icons.email_rounded,
+                          Icons.lock,
                           color: Constants().primaryColor,
                         ),
                       ),
+                      obscureText: true,
                       onChanged: (val) {
                         setState(() {
-                          email = val;
+                          password = val;
                         });
                       },
-                      validator: ((val) {
-                        return RegExp(
-                                    r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-                                .hasMatch(val!)
-                            ? null
-                            : "Please enter a valid email";
-                      })),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  TextFormField(
-                    decoration: textInputDecoration.copyWith(
-                      labelText: "Password",
-                      prefixIcon: Icon(
-                        Icons.lock,
-                        color: Constants().primaryColor,
-                      ),
-                    ),
-                    obscureText: true,
-                    onChanged: (val) {
-                      setState(() {
-                        password = val;
-                      });
-                    },
-                    validator: (val) {
-                      if (val!.length < 6) {
-                        return "password must be atleast 6 characters";
-                      } else {
-                        return null;
-                      }
-                    },
-                  ),
-                  const SizedBox(height: 20),
-                  SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          primary: Theme.of(context).primaryColor,
-                          elevation: 0,
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20))),
-                      onPressed: () {
-                        login();
+                      validator: (val) {
+                        if (val!.length < 6) {
+                          return "password must be atleast 6 characters";
+                        } else {
+                          return null;
+                        }
                       },
-                      child: const Text(
-                        "Login In",
-                        style: TextStyle(fontSize: 16, color: Colors.white),
+                    ),
+                    const SizedBox(height: 20),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            primary: Theme.of(context).primaryColor,
+                            elevation: 0,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20))),
+                        onPressed: () {
+                          login();
+                        },
+                        child: const Text(
+                          "Login In",
+                          style: TextStyle(fontSize: 16, color: Colors.white),
+                        ),
                       ),
                     ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Text.rich(TextSpan(
-                    text: "Don't have an account ? ",
-                    style: const TextStyle(color: Colors.black, fontSize: 14),
-                    children: <TextSpan>[
-                      TextSpan(
-                          text: "Register here",
-                          style: const TextStyle(
-                              color: Colors.blue,
-                              decoration: TextDecoration.underline),
-                          recognizer: TapGestureRecognizer()
-                            ..onTap = () {
-                              nextScreen(context, const RegisterPage());
-                            }),
-                    ],
-                  )),
-                ]),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Text.rich(TextSpan(
+                      text: "Don't have an account ? ",
+                      style: const TextStyle(color: Colors.black, fontSize: 14),
+                      children: <TextSpan>[
+                        TextSpan(
+                            text: "Register here",
+                            style: const TextStyle(
+                                color: Colors.blue,
+                                decoration: TextDecoration.underline),
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                nextScreen(context, const RegisterPage());
+                              }),
+                      ],
+                    )),
+                  ]),
+            ),
           ),
         ),
       ),
--- lib/pages/auth/register_page.dart
+++ lib/pages/auth/register_page.dart
@@ -1,4 +1,10 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:vechat/pages/auth/login_page.dart';
+import 'package:vechat/pages/home_page.dart';
+
+import '../../shared/constants.dart';
+import '../../widgets/widgets.dart';
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({Key? key}) : super(key: key);
@@ -8,10 +14,150 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  final formkey = GlobalKey<FormState>();
+  String email = "";
+  String password = "";
+  String fullname = "";
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("RegisterPage")),
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 80),
+            child: Form(
+              key: formkey,
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Groupies",
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const Text(
+                      "Create your account now to chat and explore",
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.w300),
+                    ),
+                    Image.asset("assets/groupie.jpg"),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      decoration: textInputDecoration.copyWith(
+                        labelText: "Full name",
+                        prefixIcon: Icon(
+                          Icons.person,
+                          color: Constants().primaryColor,
+                        ),
+                      ),
+                      onChanged: (val) {
+                        setState(() {
+                          fullname = val;
+                        });
+                      },
+                      validator: ((val) {
+                        if (val!.isNotEmpty) {
+                          return null;
+                        } else {
+                          return "Name cannot be empty";
+                        }
+                      }),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    TextFormField(
+                        decoration: textInputDecoration.copyWith(
+                          labelText: "Email",
+                          prefixIcon: Icon(
+                            Icons.email_rounded,
+                            color: Constants().primaryColor,
+                          ),
+                        ),
+                        onChanged: (val) {
+                          setState(() {
+                            email = val;
+                          });
+                        },
+                        validator: ((val) {
+                          return RegExp(
+                                      r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                                  .hasMatch(val!)
+                              ? null
+                              : "Please enter a valid email";
+                        })),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    TextFormField(
+                      decoration: textInputDecoration.copyWith(
+                        labelText: "Password",
+                        prefixIcon: Icon(
+                          Icons.lock,
+                          color: Constants().primaryColor,
+                        ),
+                      ),
+                      obscureText: true,
+                      onChanged: (val) {
+                        setState(() {
+                          password = val;
+                        });
+                      },
+                      validator: (val) {
+                        if (val!.length < 6) {
+                          return "password must be atleast 6 characters";
+                        } else {
+                          return null;
+                        }
+                      },
+                    ),
+                    const SizedBox(height: 20),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            primary: Theme.of(context).primaryColor,
+                            elevation: 0,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20))),
+                        onPressed: () {},
+                        child: const Text(
+                          "Register ",
+                          style: TextStyle(fontSize: 16, color: Colors.white),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Text.rich(TextSpan(
+                      text: "Already have a account ! ",
+                      style: const TextStyle(color: Colors.black, fontSize: 14),
+                      children: <TextSpan>[
+                        TextSpan(
+                            text: "Login now",
+                            style: const TextStyle(
+                                color: Colors.blue,
+                                decoration: TextDecoration.underline),
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                nextScreen(context, const LoginPage());
+                              }),
+                      ],
+                    )),
+                  ]),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
